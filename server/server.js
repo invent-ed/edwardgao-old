498deca802578646e2df9162eb5f30946bb273e6
Edward Gao <edsperanto@users.noreply.github.com>
2017-05-10 08:34:52 +0000

    basic server set up

---
+++ server/server.js
@@ -0,0 +1,23 @@
+// express
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// request handlers
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(cookieParser());
+
+// routes
+app.use(express.static('./public'));
+
+// start express server
+if(!module.parent) {
+	app.listen(PORT, _ => {
+		console.log(`Server listening at port ${PORT}`);
+	});
+}
