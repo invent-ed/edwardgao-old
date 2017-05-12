62c5fd437e965c0f100ab68481095488c1c78230
Edward Gao <edsperanto@users.noreply.github.com>
2017-05-12 04:23:08 +0000

    sticky side menu

---
+++ client/public/js/app.js
@@ -0,0 +1,14 @@
+(_ => {
+	const projects = document.getElementById('projects');
+	const education = document.getElementById('education');
+	const experience = document.getElementById('experience');
+	const skills = document.getElementById('skills');
+	const menu = document.getElementById('menu');
+	window.onscroll = e => {
+		let projectsTop = projects.getBoundingClientRect().top;
+		let educationTop = education.getBoundingClientRect().top;
+		let experienceTop = experience.getBoundingClientRect().top;
+		let skillsTop = skills.getBoundingClientRect().top;
+		menu.className = (projectsTop < 0) ? "static-menu" : "";
+	}
+})();
