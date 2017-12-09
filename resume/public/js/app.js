0e942e185030c88596f80cfc77775d1402548247
Edward Gao <edsperanto@users.noreply.github.com>
2017-12-08 20:35:29 -0800

    delete js folder from resume

--- resume/public/js/app.js
+++
@@ -1,8 +0,0 @@
-(_ => {
-	const projects = document.getElementById('projects');
-	const menu = document.getElementById('menu');
-	window.onscroll = e => {
-		let projectsTop = projects.getBoundingClientRect().top;
-		menu.className = (projectsTop < 1) ? "static-menu" : "";
-	}
-})();
