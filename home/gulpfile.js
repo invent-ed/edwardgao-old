ae24d7de0419a577c48d5b11ec1d9939b85c391f
Edward Gao <edsperanto@users.noreply.github.com>
2018-04-22 13:12:44 -0700

    redo homepage

--- home/gulpfile.js
+++ home/gulpfile.js
@@ -2,15 +2,15 @@ const gulp = require('gulp');
 const scss = require('gulp-sass');
 
 gulp.task('scss', () => {
-    return gulp.src('./scss/app.scss')
-        .pipe(scss({ errorLogToConsole: true }))
-        .on('error', process.exit)
-        .pipe(gulp.dest('./public/css'));
+	return gulp.src('./scss/app.scss')
+		.pipe(scss({ errorLogToConsole: true }))
+		.on('error', process.exit)
+		.pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', () => {
-    gulp.watch('./scss/**/*.scss', ['scss']);
-    gulp.watch('./gulpfile.js', process.exit);
+	gulp.watch('./scss/**/*.scss', ['scss']);
+	gulp.watch('./gulpfile.js', process.exit);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch', 'scss']);
--- home/watch.sh
+++ home/watch.sh
@@ -1,3 +1,3 @@
 while true; do
-    gulp;
+	gulp;
 done;
