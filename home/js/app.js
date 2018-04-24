2e6f35b07d2167647de968b619da60351b21cd62
Edward Gao <edsperanto@users.noreply.github.com>
2018-04-23 19:19:31 -0700

    fixed safari weird header height bug

--- home/js/app.js
+++ home/js/app.js
@@ -6,11 +6,21 @@
     const headerMenu = document.getElementById('header-menu');
     const menuSpacing = document.getElementById('menu-spacing');
     const headerSpace = document.getElementById('header-space');
+    const isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1; 
 
     // set headerSpace height
     (function followHeight() {
         headerSpace.style.height = header.offsetHeight + "px";
         setTimeout(followHeight, 5);
+    })();
+
+    // fix safari weird header height bug
+    (function safariFix() {
+        const largeMedia = '(min-device-width: 1024px) and (min-width: 1130px)';
+        const matchMedia = window.matchMedia(largeMedia).matches;
+        if(matchMedia && !isChrome) {
+            header.style.height = "9rem";
+        }
     })();
 
     // Scroll actions
@@ -20,8 +30,14 @@
         const matchMedia = window.matchMedia(largeMedia).matches;
         if(distanceY > 0 && matchMedia) {
             smallHeader();
+            if(!isChrome) {
+                header.style.height = "3rem";
+            }
         } else {
             largeHeader();
+            if(!isChrome) {
+                header.style.height = "9rem";
+            }
         }
     }
 
@@ -39,6 +55,7 @@
         menuSpacing.classList.remove('smaller');
     }
 
+    // scroll and resize actions
     window.addEventListener('scroll', scrollAction);
     window.addEventListener('resize', scrollAction);
 
