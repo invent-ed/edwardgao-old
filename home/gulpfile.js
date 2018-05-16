132d09c46051232f7a9fdb26c10301feac401e5e
Edward Gao <edsperanto@users.noreply.github.com>
2018-05-16 13:46:57 -0700

    use autoprefixer

--- home/gulpfile.js
+++ home/gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const scss = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('scss', () => {
 	return gulp.src('./scss/app.scss')
 		.pipe(scss({ errorLogToConsole: true }))
+        .pipe(autoprefixer('last 2 version'))
 		.on('error', process.exit)
 		.pipe(gulp.dest('./css'));
 });
